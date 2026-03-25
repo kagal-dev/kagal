ab4b5db0d66f823b02afdf8001a2dd369dad634d
Alejandro Mery <amery@apptly.co>
2026-03-25 22:11:45 +0000

    deps: bump @poupe/eslint-config to 0.8.4
    arrow-parens is now upstreamed, so the per-package
    overrides and GLOB_SRC imports are no longer needed.
    
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- packages/@kagal-proto/eslint.config.mjs
+++ packages/@kagal-proto/eslint.config.mjs
@@ -1,14 +1,7 @@
 // @ts-check
-import { defineConfig, GLOB_SRC } from '@poupe/eslint-config';
+import { defineConfig } from '@poupe/eslint-config';
 
 export default defineConfig(
-  // TODO: upstream arrow-parens to @poupe/eslint-config
-  {
-    files: [GLOB_SRC],
-    rules: {
-      '@stylistic/arrow-parens': ['error', 'always'],
-    },
-  },
   {
     files: ['src/gen/**'],
     rules: {
